d0c140cab8c470d17d2ad3d6e18e781f8ff8a08d
YogeshYogi1 <107339055+YogeshYogi1@users.noreply.github.com>
2022-06-16 17:53:47 +0530

    Update barrier.dart

--- flappybird/lib/barrier.dart
+++ flappybird/lib/barrier.dart
@@ -7,7 +7,6 @@ class MyBarrier extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      alignment: const Alignment(0, -0.3),
       decoration: BoxDecoration(
         color: Colors.green,
         border: Border.all(width: 5, color: Colors.green.shade900),
